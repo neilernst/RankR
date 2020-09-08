26fae56a9b6f6c2bec8b5ae4ba6cdbaa8d59aed0
alvi2496 <alvi.utsab@gmail.com>
2020-09-08 13:58:56 -0700

    initial progress on building rank interface

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -3,5 +3,9 @@ class RanksController < ApplicationController
 
     def new
         @team_members = current_student.team.students
+        @ranks = []
+        @team_members.each do |team_member|
+            @ranks << Rank.new(ranker_id: current_student.id, receiver_id: team_member.id)
+        end
     end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,8 @@
 class Student < ApplicationRecord
 
   belongs_to :team
+  has_many :authored_ranks, class_name: 'Rank', foreign_key: :ranker_id
+  has_many :received_ranks, class_name: 'Rank', foreign_key: :receiver_id
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
