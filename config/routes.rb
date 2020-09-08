6f0bbbce9d58976b36544bce218487ef97f1e629
alvi2496 <alvi.utsab@gmail.com>
2020-09-07 21:31:07 -0700

    incorporate devise with student model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :students
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :students, controllers: {
+    registrations: 'students/registrations',
+    sessions: 'students/sessions'
+  }
 end
