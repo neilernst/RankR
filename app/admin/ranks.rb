43779c403921260b63d2ca82fc818d4642d01566
alvi2496 <alvi.utsab@gmail.com>
2020-10-14 16:18:49 -0700

    add actions to rank index

--- app/admin/ranks.rb
+++ app/admin/ranks.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Rank do
 
+  permit_params :rating, :comment
+
   filter :assignment, collection: -> {
     Assignment.all.map { |assignment| [assignment.name, assignment.id] }
   }
@@ -23,6 +25,13 @@ ActiveAdmin.register Rank do
     end
     column "Comment" do |rank|
       rank.comment&.humanize
+    end
+    column :actions do |student|
+      links = []
+      links << link_to('Show', admin_rank_path(student))
+      links << link_to('Edit', edit_admin_rank_path(student))
+      links << link_to('Delete', admin_rank_path(student), method: :delete, confirm: 'Are you sure?')
+      links.join(' ').html_safe
     end
   end
   
