75e6fa87ebfe9cb3be0b18bfd53398408fcc279e
alvi2496 <alvi.utsab@gmail.com>
2020-10-17 17:01:21 -0700

    add csv import for ranks

--- app/admin/ranks.rb
+++ app/admin/ranks.rb
@@ -33,5 +33,23 @@ ActiveAdmin.register Rank do
       links.join(' ').html_safe
     end
   end
+
+  csv do
+    column "Assignment" do |rank|
+      rank.assignment&.name&.humanize 
+    end
+    column "Ranker" do |rank|
+      rank.ranker&.name 
+    end
+    column "Receiver" do |rank|
+      rank.receiver&.name 
+    end
+    column "Rating" do |rank|
+      rank.rating&.humanize
+    end
+    column "Comment" do |rank|
+      rank.comment&.humanize
+    end
+  end
   
 end
